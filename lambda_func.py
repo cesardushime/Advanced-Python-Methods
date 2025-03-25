44e4cf19fd13fa37840633816fdf5ba43b5c679a
Cesar Dushimimana <dushimezar2003@gmail.com>
2025-03-25 23:11:09 +0100

    added filter and map funcs

--- lambda_func.py
+++ lambda_func.py
@@ -23,3 +23,19 @@ print(min_val(5, 10)) # 5
 print(full_name("John", "Doe")) # John Doe
 print(is_even(10)) # True
 print(is_odd(10)) # False
+
+# filter() function is used to filter the give iterable (list, tuple, set, etc.)
+# with the help of a function that tests each element in the iterale to be true or not.
+# Syntax: filter(function, iterable)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+
+print(even_numbers) # [2, 4, 6, 8, 10]
+
+# map() function is used to apply a given function to all the items in an input_list.
+# Syntax: map(function, iterable)
+
+squared_numbers = list(map(lambda x: x ** 2, numbers))
+print(squared_numbers) # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
