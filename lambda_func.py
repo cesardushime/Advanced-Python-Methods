32cde4f6ae5a87ccb0042974651cd26b8dd94c92
Cesar Dushimimana <dushimezar2003@gmail.com>
2025-03-25 22:39:54 +0100

    lambda function syntax

---
+++ lambda_func.py
@@ -0,0 +1,9 @@
+# lambda function is a small anonymous function
+# A lambda function can take any number of arguments, but can only have one expression.
+# Syntax: lambda arguments : expression
+# The expression is executed and the result is returned.
+
+
+# double the number
+double = lambda x: x*2
+print(double(5))
