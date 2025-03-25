fa45cdb4d25380013f2e2d98caf718864fe90eb0
Cesar Dushimimana <dushimezar2003@gmail.com>
2025-03-25 22:34:14 +0100

    allowing base func to accept various arguments

--- decorators.py
+++ decorators.py
@@ -3,21 +3,21 @@
 # Decorators can be used to check for permissions, modify or track the arguments passed to a function, log the calls to a function, etc.
 
 def add_sprinkles(func):
-    def wrapper(): # wrapper function that adds sprinkles to the icecream
-        func()
-        print("You also got sprinkles 🎉")
+    def wrapper(*args, **kwargs): # accepts any number of positional and keyword arguments
+        print("You add sprinkles 🎉")
+        func(*args, **kwargs)
     return wrapper
 
 def add_cherry(func):
-    def wrapper(): # wrapper function that adds cherry to the icecream
-        func()
-        print("And some cherries 🍒")
+    def wrapper(*args, **kwargs): # wrapper function that adds cherry to the icecream
+        print("You add  cherries 🍒")
+        func(*args, **kwargs)
     return wrapper
 
 @add_sprinkles # decorator that adds sprinkles to the icecream
 @add_cherry # decorator that adds cherry to the icecream
 # The order of the decorators matters. The decorator that is defined first is the one that is executed last.
-def get_icecream():
-    print("Here is your icecream🍨")
+def get_icecream(flavor):
+    print(f"Here is your {flavor} icecream🍨")
 
-get_icecream()
+get_icecream('Chocolate') 
